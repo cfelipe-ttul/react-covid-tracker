ead9584aa0f260a06d65f4f464ed2a7ea552231e
Carlos Felipe <38431207+cfelipe-ttul@users.noreply.github.com>
2021-06-12 03:59:28 -0300

    Updates

---
+++ precache-manifest.2828d180deccd8ba22797cdbbd769930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5296efee0f966f754907db6e728feb",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "ddd75af38cd2f5e6a1fb",
+    "url": "/react-covid-tracker/static/css/main.c62b5f49.chunk.css"
+  },
+  {
+    "revision": "6361fd093eff5882aa3f",
+    "url": "/react-covid-tracker/static/js/2.a36a5c35.chunk.js"
+  },
+  {
+    "revision": "5ba854edbe2007b917d7e6843a84f808",
+    "url": "/react-covid-tracker/static/js/2.a36a5c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd75af38cd2f5e6a1fb",
+    "url": "/react-covid-tracker/static/js/main.415f1e18.chunk.js"
+  },
+  {
+    "revision": "91b8d8b30a48a12b291e",
+    "url": "/react-covid-tracker/static/js/runtime-main.ab7b88d2.js"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react-covid-tracker/static/media/spinner.235b9b3e.gif"
+  }
+]);
